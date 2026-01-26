e73364e588773e3036092c7a997ef01c78476cc4
Joao Marcelo <joaomarcelo586@gmail.com>
2026-01-25 21:15:40 -0300

    feat: Configurações iniciais do spring boot

---
+++ src/main/java/io/transportproject/Transport/TransportProjectApplication.java
@@ -0,0 +1,13 @@
+package io.transportproject.Transport;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TransportProjectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TransportProjectApplication.class, args);
+	}
+
+}
---
+++ src/test/java/io/transportproject/Transport/TransportProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package io.transportproject.Transport;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TransportProjectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
