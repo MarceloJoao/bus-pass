808748259a4ff276a945f1a4d9363cf30b477fb6
João Marcelo <51888225+MarceloJoao@users.noreply.github.com>
2026-02-02 15:45:22 -0300

    feat: criando service e controller de viagem

--- src/main/java/io/transportproject/Transport/controller/TripController.java
+++ src/main/java/io/transportproject/Transport/controller/TripController.java
@@ -1,4 +1,35 @@
 package io.transportproject.Transport.controller;
 
+import io.transportproject.Transport.entity.Trip;
+import io.transportproject.Transport.service.TripService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/api/trips")
+@RequiredArgsConstructor
 public class TripController {
+
+    private final TripService service;
+
+    
+    @PostMapping
+    public ResponseEntity<Trip> cadastrar(@RequestBody Trip trip) {
+        return ResponseEntity.ok(service.cadastrar(trip));
+    }
+
+
+    @GetMapping
+    public ResponseEntity<List<Trip>> listar() {
+        return ResponseEntity.ok(service.listarTodas());
+    }
+
+
+    @GetMapping("/{id}")
+    public ResponseEntity<Trip> buscar(@PathVariable Long id) {
+        return ResponseEntity.ok(service.buscarPorId(id));
+    }
 }
--- src/main/java/io/transportproject/Transport/service/TripService.java
+++ src/main/java/io/transportproject/Transport/service/TripService.java
@@ -1,4 +1,31 @@
 package io.transportproject.Transport.service;
 
+import io.transportproject.Transport.entity.Trip;
+import io.transportproject.Transport.repository.TripRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+@RequiredArgsConstructor
 public class TripService {
+
+    private final TripRepository repository;
+
+    // cadastrar viagem
+    public Trip cadastrar(Trip trip) {
+        return repository.save(trip);
+    }
+
+    // listar todas as viagens
+    public List<Trip> listarTodas() {
+        return repository.findAll();
+    }
+
+    // buscar por ID
+    public Trip buscarPorId(Long id) {
+        return repository.findById(id)
+                .orElseThrow(() -> new RuntimeException("Viagem não encontrada"));
+    }
 }
