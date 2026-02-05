604aaff61d0b239752502895343118a6978de48b
João Marcelo <51888225+MarceloJoao@users.noreply.github.com>
2026-02-05 14:55:48 -0300

    feat: Controller e service de User

--- src/main/java/io/transportproject/Transport/controller/UserController.java
+++ src/main/java/io/transportproject/Transport/controller/UserController.java
@@ -1,18 +1,35 @@
 package io.transportproject.Transport.controller;
 
-import io.transportproject.Transport.entity.Trip;
-import io.transportproject.Transport.service.TripService;
+import io.transportproject.Transport.entity.User;
+import io.transportproject.Transport.service.UserService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
-
 @RestController
-@RequestMapping("users")
-@RequestArgsConstructor
+@RequestMapping("/api/users")
+@RequiredArgsConstructor
 public class UserController {
-    
-    
+
+    private final UserService service;
+
+    // POST - cadastrar usuário
+    @PostMapping
+    public ResponseEntity<User> cadastrar(@RequestBody User user) {
+        return ResponseEntity.ok(service.cadastrar(user));
+    }
+
+    // GET - listar usuários
+    @GetMapping
+    public ResponseEntity<List<User>> listar() {
+        return ResponseEntity.ok(service.listarTodos());
+    }
+
+    // GET - buscar usuário por id
+    @GetMapping("/{id}")
+    public ResponseEntity<User> buscar(@PathVariable Long id) {
+        return ResponseEntity.ok(service.buscarPorId(id));
+    }
 }
--- src/main/java/io/transportproject/Transport/service/UserService.java
+++ src/main/java/io/transportproject/Transport/service/UserService.java
@@ -1,4 +1,31 @@
 package io.transportproject.Transport.service;
 
+import io.transportproject.Transport.entity.User;
+import io.transportproject.Transport.repository.UserRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+@RequiredArgsConstructor
 public class UserService {
+
+    private final UserRepository repository;
+
+    // cadastrar usuário
+    public User cadastrar(User user) {
+        return repository.save(user);
+    }
+
+    // listar todos os usuários
+    public List<User> listarTodos() {
+        return repository.findAll();
+    }
+
+    // buscar usuário por ID
+    public User buscarPorId(Long id) {
+        return repository.findById(id)
+                .orElseThrow(() -> new RuntimeException("Usuário não encontrado"));
+    }
 }
